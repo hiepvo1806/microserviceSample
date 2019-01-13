b7d82cf88506e3ee526ae47110ea85f2b9c44c73
hiepvo <k3c6.hiep@gmail.com>
2019-01-13 19:38:17 +0700

    updated with multiple service

---
+++ MicroserviceSample/MultipleService/Controllers/MultipleController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading;
+
+namespace MultipleService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MultipleController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Plus service type";
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] MultipleModel value)
+        {
+            try
+            {
+                var result = value.X * value.Y;
+                Thread.Sleep(5000);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.ToString());
+            }
+        }
+    }
+    public class MultipleModel
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+
+}
