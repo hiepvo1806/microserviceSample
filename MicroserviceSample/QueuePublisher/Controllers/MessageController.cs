9522867b2d622d1d467c8e12bb4262fee1ee48fb
Hiep Jeff Vo <k3c6.hiep@gmail.com>
2019-01-31 17:35:49 +0700

    updated with queue publisher

---
+++ MicroserviceSample/QueuePublisher/Controllers/MessageController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace QueuePublisher.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "MessageController ok!" };
+        }
+
+        [HttpPost]
+        public ActionResult<IEnumerable<string>> Post([FromBody] PostContent content)
+        {
+            return new[] { $"MessageController HttpPost! : {JsonConvert.SerializeObject(content)}" };
+        }
+    }
+
+    public class PostContent
+    {
+        public string Content { get; set; }
+    }
+}
